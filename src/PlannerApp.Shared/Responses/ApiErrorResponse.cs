1177a1f9054054a32173812203e399aee2a894f6
kchrosci <kamil.chroscicki.stud@pw.edu.pl>
2021-06-06 14:57:11 +0200

    Create api response models

---
+++ src/PlannerApp.Shared/Responses/ApiErrorResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace PlannerApp.Shared.Responses
+{
+	public class ApiErrorResponse
+	{
+		public string message { get; set; }
+		public string[] errors { get; set; }
+		public bool isSuccess { get; set; }
+	}
+}
+
